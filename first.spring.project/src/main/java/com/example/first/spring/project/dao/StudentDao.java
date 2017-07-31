093398c33cab5a34dbdefeb7e92974d778d4d075
Nikola Jović <nikolajovic@live.com>
2017-07-31 18:17:48 +0200

    Update StudentDao.java

--- first.spring.project/src/main/java/com/example/first/spring/project/dao/StudentDao.java
+++ first.spring.project/src/main/java/com/example/first/spring/project/dao/StudentDao.java
@@ -8,32 +8,4 @@ import org.springframework.stereotype.Component;
 import com.example.first.spring.project.entity.Student;
 @Component
 public interface StudentDao extends CrudRepository<Student, Integer> {
-/*	private final HashMap<Integer, Student> students = new HashMap<Integer, Student>();
-	private int brojac;
-
-	public StudentDao() {
-		brojac = 0;
-	}
-	
-	public HashMap<Integer, Student> getStudents(){
-		return students;
-	}
-	
-	public Student get(int id) {
-		return students.get(id);
-	}
-
-	public void add(Student student) {
-		System.out.println("PROBA");
-		student.setId(brojac);
-		students.put(brojac++, student);
-	}
-
-	public void remove(int id) {
-		students.remove(id);
-	}
-
-	public void update(Student student) {
-		students.put(student.getId(), student);
-	}*/
 }
